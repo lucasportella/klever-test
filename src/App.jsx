ac62d5cfb2f4d042f52f2c2c623f5fde54f764c5
lucas <lucasportellaagu@gmail.com>
2022-03-18 14:17:34 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import AddToken from './Pages/AddToken';
 import EditToken from './Pages/EditToken';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <AddToken />
-      <EditToken />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="addtoken" element={<AddToken />} />
+        <Route path="edittoken" element={<EditToken />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
