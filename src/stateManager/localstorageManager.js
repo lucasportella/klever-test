3d57c286b006ae4591b8e9112fa11073bb6443a8
lucas <lucasportellaagu@gmail.com>
2022-03-21 00:58:00 -0300

    storage all tokens in upper case

--- src/stateManager/localstorageManager.js
+++ src/stateManager/localstorageManager.js
@@ -1,27 +1,29 @@
 const loadAllSavedTokens = () => JSON.parse(localStorage.getItem('wallet'));
 
-const storageNewToken = (payload) => {
-  if (!localStorage.length) {
-    localStorage.setItem('wallet', JSON.stringify([payload]));
+const storageNewToken = ({ token, balance }) => {
+  const formattedPayload = { token: token.toUpperCase(), balance };
+  if (!loadAllSavedTokens()) {
+    localStorage.setItem('wallet', JSON.stringify([formattedPayload]));
   } else {
     const wallet = loadAllSavedTokens();
-    if (wallet.some((currency) => currency.token === payload.token)) {
+    if (wallet.some((currency) => currency.token === formattedPayload.token)) {
       throw new Error('Token already exists on wallet');
     }
-    wallet.push(payload);
+    wallet.push(formattedPayload);
     localStorage.setItem('wallet', JSON.stringify(wallet));
   }
 };
 
-const storageEditToken = (prevState, payload) => {
+const storageEditToken = (prevState, { token, balance }) => {
+  const formattedPayload = { token: token.toUpperCase(), balance };
   const wallet = loadAllSavedTokens();
   if (wallet
-    .some((currency) => currency.token === payload.token
-    && prevState.token !== payload.token)) {
+    .some((currency) => currency.token === formattedPayload.token
+    && prevState.token !== formattedPayload.token)) {
     throw new Error('Token already exists on wallet');
   }
   const updatedWallet = wallet
-    .map((currency) => (currency.token === prevState.token ? payload : currency));
+    .map((currency) => (currency.token === prevState.token ? formattedPayload : currency));
   localStorage.setItem('wallet', JSON.stringify(updatedWallet));
 };
 
