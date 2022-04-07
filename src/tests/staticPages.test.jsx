976aefec7d0234007a2ada739e74ce11a6851728
lucas <lucasportellaagu@gmail.com>
2022-04-07 13:14:57 -0300

    EditToken page static test

--- src/tests/staticPages.test.jsx
+++ src/tests/staticPages.test.jsx
@@ -66,4 +66,43 @@ describe('test pages static content', () => {
       expect(element).toBeVisible();
     });
   });
+
+  test('if EditToken page elements are correctly rendered', () => {
+    render(
+      <MemoryRouter initialEntries={['/', { pathname: '/edittoken', state: { token: 'BTC', balance: '2.32' } }]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const title = screen.getByAltText(/Klever logo/i);
+    const headerTitle = screen.getByText(/Wish Wallet/);
+    const headerLogo = screen.getByAltText(/shooting star/i);
+    const subHeaderTitle = screen.getByText(/Edit Token/);
+    const backBtn = screen.getByText(/Voltar/);
+    const tokenLabel = screen.getByLabelText(/Token/);
+    const balanceLabel = screen.getByLabelText(/Balance/);
+    const saveBtn = screen.getByText(/Save/);
+    const removeBtn = screen.getByText(/Remove/);
+
+    const pageElements = [
+      title,
+      headerTitle,
+      headerLogo,
+      subHeaderTitle,
+      backBtn,
+      tokenLabel,
+      balanceLabel,
+      saveBtn,
+      removeBtn,
+    ];
+
+    pageElements.forEach((element) => {
+      expect(element).toBeVisible();
+    });
+
+    const tokenInput = screen.getByDisplayValue('BTC');
+    const balanceInput = screen.getByDisplayValue('2.32');
+    expect(tokenInput).toBeVisible();
+    expect(balanceInput).toBeVisible();
+  });
 });
