56e66ce7f1a9e25a9b61a0ba3c27889fc3a8c30a
lucas <lucasportellaagu@gmail.com>
2022-04-07 19:09:57 -0300

    fix: subheader center position in bigger screens

--- src/Components/SharedComponents/SubHeader.jsx
+++ src/Components/SharedComponents/SubHeader.jsx
@@ -4,13 +4,15 @@ import shootingStar from '../../assets/shooting-star-yellow.png';
 
 function SubHeader({ child }) {
   return (
-    <div className="subHeader">
-      <div className="starAndTitle">
-        <img className="shootingStar" src={shootingStar} alt="shooting star" />
-        <h2>Wish Wallet</h2>
-      </div>
-      <div className="btnFixedArea">
-        {child}
+    <div className="subHeaderFather">
+      <div className="subHeader">
+        <div className="starAndTitle">
+          <img className="shootingStar" src={shootingStar} alt="shooting star" />
+          <h2>Wish Wallet</h2>
+        </div>
+        <div className="btnFixedArea">
+          {child}
+        </div>
       </div>
     </div>
   );
